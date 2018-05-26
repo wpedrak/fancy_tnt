93b7c42635f470c4cab99c23869230047c9f5e45
Wojciech Pędrak <woj123@gmail.com>
2018-05-26 19:50:58 +0200

    part of make_floor function

--- init.lua
+++ init.lua
@@ -17,17 +17,55 @@ local function copy2(pos)
     end
 end
 
+local function make_floor(pos)
+	local meta        = minetest.get_meta(pos)
+	local floor_size  = meta:get_int('punch_counter')
+	local pos1   	  = vector.subtract(pos, {x=floor_size, y=0, z=floor_size})
+	local pos2 		  = vector.add     (pos, {x=floor_size, y=0, z=floor_size})
+	local vm          = minetest.get_voxel_manip()
+	local emin, emax  = vm:read_from_map(pos1, pos2)
+	local pos_top     = vector.add(pos, {x=0, y=1, z=0})
+	local node_top    = minetest.get_node_or_nil(pos_top)
+    local c_node_name = minetest.get_content_id(node_top.name)
+    
+    if node_top then
+    
+    else
+    
+    end
+    
+	-- minetest.chat_send_all(dump(node_top))
+	minetest.remove_node(pos)
+end
+
+local function update_description(pos)
+	local meta = minetest.get_meta(pos)
+	local counter = meta:get_int('punch_counter')
+	meta:set_string("infotext", counter)
+end
+
+
 minetest.register_node("fancy_tnt:copy", {
     description = "Copying TNT",
     tiles = {"tnt_up.png", "tnt_bot.png", "tnt8.png"},
     groups = {dig_immediate = 2, mesecon = 2, tnt = 1, flammable = 5},
     sounds = default.node_sound_wood_defaults(),
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		meta:set_int('punch_counter', 0)
+		update_description(pos)
+	end,
     on_punch = function(pos, node, puncher, pointed_thing)
+		local meta = minetest.get_meta(pos)
+		local counter = meta:get_int('punch_counter')
+		meta:set_int('punch_counter', counter+1)
+		update_description(pos)
         if puncher:get_wielded_item():get_name() == "default:torch" then
+			meta:set_string("infotext", "")
             minetest.swap_node(pos, {name="fancy_tnt:copy_burning"})
             minetest.registered_nodes["fancy_tnt:copy_burning"].on_construct(pos)
         end
-    end
+    end,
 })
 
 minetest.register_node("fancy_tnt:copy_burning", {
@@ -38,7 +76,8 @@ minetest.register_node("fancy_tnt:copy_burning", {
     on_timer = function(pos, elapsed)
         minetest.sound_play("tnt_explode", {pos = pos, gain = 1.5,
         max_hear_distance = 128})
-        copy2(pos)
+        -- copy2(pos)
+		make_floor(pos)
     end,
     -- unaffected by explosions
     on_blast = function() end,
