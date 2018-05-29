bc2b5fb868c6e8f9690f9d7f2faa0e63c435d721
wpedrak <woj123@gmail.com>
2018-05-29 11:08:57 +0200

    changed crafting of copy_tnt + hammer crafting

--- crafting.lua
+++ crafting.lua
@@ -1,8 +1,17 @@
 minetest.register_craft({
     output = 'fancy_tnt:copy 1',
     recipe = {
-        {'', 'group:stone', ''},
-        {'', 'tnt:tnt',   ''},
-        {'', 'group:stone', ''},
+        {'',            'group:stone', ''           },
+        {'group:stone', 'tnt:tnt',     'group:stone'},
+        {'',            '',             ''          },
+    }
+})
+
+minetest.register_craft({
+    output = 'fancy_tnt:hammer 1',
+    recipe = {
+        {'group:stone', 'group:stone',   ''           },
+        {'group:stone', 'group:stone',   'group:stone'},
+        {'',            'group:stick', ''           },
     }
 })
