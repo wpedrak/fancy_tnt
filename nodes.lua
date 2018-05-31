3a9b0bdd08cdc9a4dfec7596d9deecc37dacc2ef
Wojciech Pędrak <woj123@gmail.com>
2018-05-31 17:22:52 +0200

    commented formspec

--- nodes.lua
+++ nodes.lua
@@ -100,6 +100,45 @@ local function get_meta_xyz(pos)
            meta:get_int('z')
 end
 
+-- local tnt_formspec_context = {}
+
+--         land_formspec_context[name] = {id = param}
+
+--         minetest.show_formspec(name, "fancy_tnt:edit",
+--                 "size[4,4]" ..
+--                 "field[1,1;3,1;plot;Plot Name;]" ..
+--                 "field[1,2;3,1;owner;Owner;]" ..
+--                 "button_exit[1,3;2,1;exit;Save]")
+
+
+
+-- --
+-- -- Step 2) retrieve context when player submits the form
+-- --
+-- minetest.register_on_player_receive_fields(function(player, formname, fields)
+--     if formname ~= "mylandowner:edit" then
+--         return false
+--     end
+
+--     -- Load information
+--     local context = land_formspec_context[player:get_player_name()]
+
+--     if context then
+--         minetest.chat_send_player(player:get_player_name(), "Id " ..
+--                 context.id .. " is now called " .. fields.plot ..
+--                 " and owned by " .. fields.owner)
+
+--         -- Delete context if it is no longer going to be used
+--         land_formspec_context[player:get_player_name()] = nil
+
+--         return true
+--     else
+--         -- Fail gracefully if the context does not exist.
+--         minetest.chat_send_player(player:get_player_name(),
+--                 "Something went wrong, try again.")
+--     end
+-- end)
+
 minetest.register_node("fancy_tnt:copy", {
     description = "Copying TNT",
     tiles = {"tnt_up.png", 
